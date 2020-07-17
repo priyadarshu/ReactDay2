fdc66c887b7583e9d4e31f8c7622f41c1e011655
Priyadarshini Chettiar <45838555+priyadarshu@users.noreply.github.com>
2020-07-17 20:39:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import TodoItem from "./components/TodoItem";
+import todosData from "./components/todosData";
+import "./styles.css";
+
+class App extends React.Component {
+  render() {
+    const todoItems = todosData.map(item => (
+      <TodoItem key={item.id} item={item} />
+    ));
+    return <div className="todo-list">{todoItems}</div>;
+  }
+}
+
+export default App;
